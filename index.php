c9506b14ee42a4a2024ed0c8bff47d97fb8f77d5
damarest <113396451+damarest@users.noreply.github.com>
2022-11-04 19:34:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
-	 
+	 <meta charset="UTF-8"> 
     <head>
-	  <meta charset="UFT-8">  
+	   
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Prédio MDPS</title>
 	   
@@ -135,7 +135,7 @@
 						
                             <div>
                                 
-                                <strong>$4000.000,00 Compra</strong>
+                                <strong>$400.000,00 Compra</strong>
                                 
                                 <p>Ótimos apartamentos com 2 dormitorios,
                                    com sala espaçosa, cozinha,
