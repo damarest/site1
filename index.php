59536424d4ee08fc83ec59e692d4a5d3db421466
damarest <113396451+damarest@users.noreply.github.com>
2022-11-04 18:43:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	    <meta charset="UTF-8"> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Prédio MDPS</title>
-	   
+	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">  
 		
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/fontawesome.min.css">
